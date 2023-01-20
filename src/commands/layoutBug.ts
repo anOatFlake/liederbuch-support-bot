b7abf7af25e0df58fe8b63661277278c384696f7
AnOatFlake <annika@flock.cc>
2023-01-20 19:40:33 +0100

    added bug command for layout bugs

---
+++ src/commands/layoutBug.ts
@@ -0,0 +1,50 @@
+import {
+  CommandInteraction,
+  Client,
+  ApplicationCommandOptionType,
+  ApplicationCommandOption,
+} from 'discord.js';
+import { createIssue } from '../util/githubUtil';
+import { Command } from '../models/command';
+import { IssueType } from '../models/issueType';
+
+const songName: ApplicationCommandOption = {
+  type: ApplicationCommandOptionType.String,
+  name: 'song-name',
+  description: 'which song has a layout bug',
+  required: true,
+};
+const bugDescription: ApplicationCommandOption = {
+  type: ApplicationCommandOptionType.String,
+  name: 'bug-description',
+  description: 'describe here the bug that you encountered',
+  required: false,
+};
+const linkToScreenshot: ApplicationCommandOption = {
+  type: ApplicationCommandOptionType.String,
+  name: 'link-to-screenshot',
+  description: 'optional: include a link to a screenshot of your bug',
+  required: false,
+};
+
+export const layoutBug: Command = {
+  name: 'bug',
+  description: 'decribe a new bug in the liederbuch app',
+  options: [songName, bugDescription, linkToScreenshot],
+  run: async (client: Client, interaction: CommandInteraction) => {
+    //@ts-ignore
+    const title = interaction.options.getString('song-name');
+    //@ts-ignore
+    const description = interaction.options.getString('bug-description');
+    //@ts-ignore
+    const link = interaction.options.getString('link-to-screenshot');
+    const content = 'new bug-issue has been created';
+
+    await createIssue(IssueType.LAYOUT_BUG, title, description, link);
+
+    await interaction.followUp({
+      ephemeral: true,
+      content,
+    });
+  },
+};
